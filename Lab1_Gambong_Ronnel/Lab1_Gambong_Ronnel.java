a610ef75964824562bdc9438bec69f282837e7b1
ronzron <75774813+ronzron@users.noreply.github.com>
2021-06-02 16:41:49 +0800

    ff

---
+++ Lab1_Gambong_Ronnel/Lab1_Gambong_Ronnel.java
@@ -0,0 +1,11 @@
+package Lab1_Gambong_Ronnel;
+
+public class Lab1_Gambong_Ronnel {
+    public static void main(String[] args) {
+        System.out.println("\"I am writing a program that will display \"text\" with escape characters.");
+        System.out.println("\t At the beginning of this sentence it has tab escape character.");
+        System.out.println(
+                "\n \nThere are two new line before this sentence and you also ahve to include \'single \n quote\' and \\back slash\\ characters in this sentence.\" ");
+
+    }
+}
---
+++ Lab1_Gambong_Ronnel/variable.java
@@ -0,0 +1,30 @@
+package Lab1_Gambong_Ronnel;
+
+public class variable {
+
+    public static void main(String[] args) {
+        double gross = 0.0;
+        int i = 0x11;
+        int o = 067;
+        int age = 1;
+        boolean f = false;
+        char d = 'a';
+        String nm = "noname";
+
+        // Gross statement output that I called from the variable
+        System.out.println("Gross sale = " + gross);
+        // Age value for the statement
+        System.out.println("Age = " + age);
+        // Final statement output
+        System.out.println("Final = " + f);
+
+        // Statement for location which is a literal
+        System.out.println("Location = " + i);
+        // Old file statement
+        System.out.println("Old file = " + o);
+        // Display default values
+        System.out.println("Default = " + d);
+        // Display noname values
+        System.out.println("Name = " + nm);
+    }
+}
